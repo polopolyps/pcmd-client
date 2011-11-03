004b10a6ece92b8b6b363b32f606904a511bd1a4
Surasin Tancharoen <surasin.tancharoen@atex.com>
2011-11-03 10:20:53 +0000

    improvement: auto get PEAR path

--- src/main/resources/script/pcmd.bat
+++ src/main/resources/script/pcmd.bat
@@ -1,12 +1,11 @@
+@echo off
 echo off
-SETLOCAL ENABLEDELAYEDEXPANSION 
 
-rem TODO 
-rem pcmdwindow_getpearpath.bat
-rem SET PCMD_PEAR_PATH=C:\projects\avid\polopoly\pear\
+SET PCMD_CURRENT_DIR=%~dp0
 
 rem hot fix
-SET PCMD_PEAR_PATH=%PEAR_PATH%
+call pcmd_getpearpath.bat
+SETLOCAL ENABLEDELAYEDEXPANSION 
 
 SET PCMD_OPTION=
 SET PCMD_CLIENT_LIBS=
@@ -23,37 +22,36 @@ IF NOT "" == "%POLOPOLY_PASSWORD%" (
 	SET PCMD_OPTION=%PCMD_OPTION% --password=%POLOPOLY_PASSWORD%
 )
 
-IF EXIST "..\custom-lib\*.jar" (
-	FOR /F "tokens=*" %%G IN ('dir /b ..\custom-lib\*.jar') DO (
-		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!..\custom-lib\%%G;
+IF EXIST "%PCMD_CURRENT_DIR%..\custom-lib\*.jar" (
+	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_CURRENT_DIR%..\custom-lib\*.jar') DO (
+		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_CURRENT_DIR%..\custom-lib\%%G;
 	)
 )
 
-IF EXIST "..\lib\*.jar" (
-	FOR /F "tokens=*" %%G IN ('dir /b ..\lib\*.jar') DO (
-		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!..\lib\%%G;
+IF EXIST "%PCMD_CURRENT_DIR%..\lib\*.jar" (
+	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_CURRENT_DIR%..\lib\*.jar') DO (
+		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_CURRENT_DIR%..\lib\%%G;
 	)
 )
 
-IF EXIST "%PCMD_PEAR_PATH%..\custom\client-lib\*.jar" (
-	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_PEAR_PATH%..\custom\client-lib\*.jar') DO (
-		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_PEAR_PATH%..\custom\client-lib\%%G;
+IF EXIST "%PCMD_PEAR_PATH_LOCAL%..\custom\client-lib\*.jar" (
+	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_PEAR_PATH_LOCAL%..\custom\client-lib\*.jar') DO (
+		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_PEAR_PATH_LOCAL%..\custom\client-lib\%%G;
 	)
 )
 
-IF EXIST "%PCMD_PEAR_PATH%..\jar-repository\policy-lib\*.jar" (
-	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_PEAR_PATH%..\jar-repository\policy-lib\*.jar') DO (
-		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_PEAR_PATH%..\jar-repository\policy-lib\%%G;
+IF EXIST "%PCMD_PEAR_PATH_LOCAL%jar-repository\policy-lib\*.jar" (
+	FOR /F "tokens=*" %%G IN ('dir /b %PCMD_PEAR_PATH_LOCAL%jar-repository\policy-lib\*.jar') DO (
+		SET PCMD_CLIENT_LIBS=!PCMD_CLIENT_LIBS!%PCMD_PEAR_PATH_LOCAL%jar-repository\policy-lib\%%G;
 	)
 )
 
 IF "%1" == "--verbose" (
+	echo PCMD_PEAR_PATH_LOCAL:%PCMD_PEAR_PATH_LOCAL%
+	echo JAVA_OPTS:%JAVA_OPTS% 
+	echo PCMD_CLIENT_LIBS:%PCMD_CLIENT_LIBS% 
+	echo PCMD_OPTION:%PCMD_OPTION% 
 	echo "Full classpath consists of: %PCMD_CLIENT_LIBS%"
 )
 
-@echo "JAVA_OPTS=" %JAVA_OPTS% 
-@echo "PCMD_CLIENT_LIBS=" %PCMD_CLIENT_LIBS% 
-@echo "PCMD_OPTION=" %PCMD_OPTION% 
-
-echo on
 java %JAVA_OPTS% -cp %PCMD_CLIENT_LIBS% com.polopoly.ps.pcmd.Main%PCMD_OPTION% %*
